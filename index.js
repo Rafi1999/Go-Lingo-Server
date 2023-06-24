dba172768698c8e2b8d81a7ea2b7f001d6ac6e8d
Al Rafi <60866655+Rafi1999@users.noreply.github.com>
2023-06-24 23:16:18 +0600

    selected class

--- index.js
+++ index.js
@@ -48,6 +48,15 @@ async function run() {
     })
 
     //selected collection
+    app.get('/selected',async(req,res)=>{
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = {email : email};
+      const result = await selectedCollection.find(query).toArray();
+      res.send(result);
+    });
     app.post('/selected',async(req,res)=>{
       const item = req.body;
       console.log(item);
