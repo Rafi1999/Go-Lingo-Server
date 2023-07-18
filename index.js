1b6ca24f6fcb6b44adeb9ec4a5ee8e28bbdeab6c
Al Rafi <60866655+Rafi1999@users.noreply.github.com>
2023-07-19 01:20:23 +0600

    fully done except hosting

--- index.js
+++ index.js
@@ -234,6 +234,10 @@ async function run() {
       const result = await paymentCollection.find(query).toArray();
       res.send(result);
     });
+    app.get('/popular', async (req, res) => {
+      const result = await paymentCollection.find().toArray();
+      res.send(result);
+    });
 
     /*
 
