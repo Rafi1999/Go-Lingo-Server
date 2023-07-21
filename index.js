0186512f41c5afc10c3aed5fd1f86eb62bbb8ca7
Al Rafi <60866655+Rafi1999@users.noreply.github.com>
2023-07-21 21:44:15 +0600

    first commit

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
     const usersCollection = client.db("golingoDb").collection("users");
     const classesCollection = client.db("golingoDb").collection("classes");
 
